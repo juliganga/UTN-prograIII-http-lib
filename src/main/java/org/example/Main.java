79c90f6be64315f45315b4112e349f1adac3ad3b
Tomas Silva <toto9116@gmail.com>
2025-05-10 01:40:44 -0300

    Hacer algunas correcciones y usar HttpClient en lugar de HttpURLConnection por estar deprecado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import com.google.gson.JsonObject;
 import org.example.Exceptions.BadRequestException;
 import org.example.Exceptions.IncorrectRequestType;
 import org.example.Modelo.BackendRequest;
@@ -10,13 +9,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         //BackendRequest pedido = new BackendRequest("https://postman-echo.com","/get");
-        BackendRequest pedido2 = new BackendRequest("https://postman-echo.com","/GET");
+        BackendRequest pedido2 = new BackendRequest("https://postman-echo.com","/get");
         try {
             //System.out.println(pedido2.searchData(GetRequestType.ARRAY));
-            System.out.println(pedido2.searchData(GetRequestType.ARRAY));
-        } catch (BadRequestException | IOException | IncorrectRequestType e)
+            //JsonObject object = new JsonObject();
+            //object.addProperty("client_id","1");
+            System.out.println(pedido2.prettifyData(pedido2.searchData(),GetRequestType.OBJECT));
+            //pedido2.makeChangeViaForm(object,"POST");
+        } catch (BadRequestException | IOException | InterruptedException | IncorrectRequestType e)
         {
             System.out.println(e.getMessage());
         }
